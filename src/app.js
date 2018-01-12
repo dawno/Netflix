07ad2bcc38e93f32e62043db21d9582877d49f1f
Dawno <utkarshsinghcs@gmail.com>
2018-01-12 20:24:00 +0530

    Netflix

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import {
+  Text,View,StyleSheet
+} from 'react-native'
+import List from './components/List'
+import Slide from './components/Slider'
+class App extends Component{
+  render(){
+    return(
+      <View style = {[{flex :1}, styles.container]}>
+       <Slide/>
+      <List/>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor : 'black'
+  }
+})
+export default App
